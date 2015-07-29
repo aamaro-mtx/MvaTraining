ffb2472a7f5e3c031fed3ee9bd5f8b9b6fb5b680
Abel Amaro <amarotux@gmail.com>
2015-07-29 12:49:18 -0400

    Test Commit

---
+++ Mva.Training/Models/Model.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Mva.Training.Models
+{
+    public class Model
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public int Age { get; set; }
+        public DateTime BirthDate { get; set; }
+    }
+}
